670ed299e8ddf74e1f086871141caa1af0f2e717
samuelblebo <samuel.blebo@yahoo.com>
2023-10-19 12:33:59 +0000

    .

--- src/Components/All.js
+++ src/Components/All.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+
 import Menu from "../Components/Menu";
 
 export default function All() {
+  const [dates, setDates] = useState([]); // Changed the state variable name
+
+  const getAll = () => {
+    axios
+      .get("https://birthbuddy-server.vercel.app/dates")
+      .then((res) => {
+        const jsonData = res.data; // Use res.data to access JSON
+        console.log(jsonData);
+        setDates(jsonData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  function formatDate(eventDateStr) {
+    const eventDate = new Date(eventDateStr);
+    const day = eventDate.getDate().toString().padStart(2, "0"); // Ensure two digits
+    const month = (eventDate.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-based
+    const year = eventDate.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  }
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
   return (
     <div className="h-[700px] w-[380px] bg-[#D9D9D9] rounded-[18px] px-[35px] py-[50px] relative">
       {/* text and add new */}
@@ -27,6 +57,21 @@ export default function All() {
       </div>
       <div className="absolute bottom-0 mb-[20px] mx-[6%]">
         <Menu />
+      </div>
+
+      <div className="h-[400px] overflow-y-auto no-scrollbar ">
+        {" "}
+        {/* Set a fixed height and add overflow-y */}
+        <ul>
+          {dates.map((date) => (
+            <div key={date.id} className="bg-[#fff] mt-[20px] p-[20px] rounded">
+              <li>{date.name}</li>
+              <li>{formatDate(date.event_date)}</li>
+              <li>{date.event_type}</li>
+              <li>{date.mobile_number}</li>
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
